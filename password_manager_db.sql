22ed51fe47524f1f7e2869017d769bf455b65a7f
GiBauF <79844257+GiBauF@users.noreply.github.com>
2025-10-23 13:41:08 +0200

    Added password_vault table

--- password_manager_db.sql
+++ password_manager_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 07, 2025 at 02:05 PM
+-- Generation Time: Oct 23, 2025 at 01:15 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -34,9 +34,15 @@ CREATE TABLE `password_vault` (
   `WebsiteURL` varchar(255) DEFAULT NULL,
   `Username` varchar(255) NOT NULL,
   `EncryptedPassword` text NOT NULL,
-  `CreatedAt` timestamp NOT NULL DEFAULT current_timestamp(),
-  `UpdatedAt` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+  `CreatedAt` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `password_vault`
+--
+
+INSERT INTO `password_vault` (`VaultID`, `UserID`, `WebsiteName`, `WebsiteURL`, `Username`, `EncryptedPassword`, `CreatedAt`) VALUES
+(1, 13, 'string', 'string', 'string', 'c3RyaW5n', '2025-10-23 09:15:05');
 
 -- --------------------------------------------------------
 
@@ -56,8 +62,18 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`UserID`, `Email`, `PasswordHash`, `CreatedAt`) VALUES
-(1, 'string', '$2a$11$yNDL3WXQ7/pksPL4lOcK3u8WdDtZ4UTV3DbZDiCu9KZ56yYuNkeDm', '2025-10-07 09:48:00'),
-(2, 'string1', '$2a$11$4acglVylSpqgOwSll/Mk0OAJJ1TA40rqz9oNhXysEsDLm.faGTrMG', '2025-10-07 09:49:51');
+(2, 'string1', '$2a$11$4acglVylSpqgOwSll/Mk0OAJJ1TA40rqz9oNhXysEsDLm.faGTrMG', '2025-10-07 09:49:51'),
+(3, 'string2', '$2a$11$mm3ngYpJGDqY1spiK5c.eOGkIm2HgwRe4aMDvbKaGE0gGVGroh.ZW', '2025-10-10 05:58:49'),
+(4, 'string4', '$2a$11$8tMvLvb4IWaQdqep3EEetuaPT1oIpVRfW6mJQdfkB4jlO5kjTXuHi', '2025-10-10 07:34:22'),
+(5, 'string8', '$2a$11$3t3.z8yj4q7Ki8IBLfwDoOBOGVZMoe0slxj2dweVB7Wb5ZL3UNXV6', '2025-10-13 13:21:42'),
+(6, 'string9', '$2a$11$oH.LZ/YQhzcrnJNynkQWxuIF.sYIgyQ2OGwJ.zIOJP0uiNdtOZq0m', '2025-10-13 13:26:09'),
+(7, 'string99', '$2a$11$/rltbeMUzJT4uJbA9ub2sOwETCjjH.X/JiO/n.P5alARpd.ivOwXK', '2025-10-15 15:08:54'),
+(8, 'string10', '$2a$11$DhryHywIm4IVAQ2LEFzvWuKj.2AR6xk.PDGyP8pixqahtpwf065qC', '2025-10-20 05:58:27'),
+(9, 'string@string.com', '$2a$11$2yia0h62Mfe8gqLLkkm6QuNLVF9Wcyd7q0dL6edDfKAy.UTn/ge1K', '2025-10-20 07:05:50'),
+(10, 'string123@string.com', '$2a$11$Ori1.6nV5eeCcRE6esIkXe6ziqPjPLVLTaBu9zplJTrZR0Fu/0sMq', '2025-10-20 07:09:35'),
+(11, 'string10@gmail.com', '$2a$11$caqKdoXslXjtf5UxmnHtiubBZ7lIE/XAbbfJc7PUm2I5KZkJUeFOu', '2025-10-21 08:52:26'),
+(12, 'string1234@gmail.com', '$2a$11$/fefzlte2u3i2XoKe6zNUeYdO5AmrjP1W14sCHlnzyTMXhATn.m8m', '2025-10-21 09:52:11'),
+(13, 'string', '$2a$11$QWlFQIpsXVDqBe2tZM2Gsemh9DsYtIZ75lqI1YQrwJQlg2OZ45bu6', '2025-10-21 11:04:05');
 
 --
 -- Indexes for dumped tables
@@ -85,13 +101,13 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `password_vault`
 --
 ALTER TABLE `password_vault`
-  MODIFY `VaultID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `VaultID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `UserID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `UserID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- Constraints for dumped tables
